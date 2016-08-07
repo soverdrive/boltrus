235a683174597d0d454dc19fdec621d204ec2ad2
albert-widi <albert.widiatmoko@gmail.com>
2016-08-07 20:40:05 +0700

    Init boltrus

---
+++ README.md
@@ -0,0 +1,45 @@
+[Boltdb](https://github.com/boltdb/bolt) Hooks for [Logrus](https://github.com/Sirupsen/logrus) <img src="http://i.imgur.com/hTeVwmJ.png" width="40" height="40" alt=":walrus:" class="emoji" title=":walrus:"/>
+-------
+
+Hi, Boltrus is a BoltDB hooks for Logrus.
+
+Install
+-------
+```shell
+$ go get github.com/albert-widi/boltrus
+```
+
+Usage
+------
+```go
+package main
+
+import (
+  "github.com/Sirupsen/logrus"
+	"github.com/weekface/mgorus"
+)
+
+log := logrus.New()
+hooker, err := boltrus.NewHook("files/logger/")
+
+if err == nil {
+  log.Hooks.Add(hooker)
+}
+
+log.WithFields(logrus.Fields{
+	"name": "albert",
+	"job":  "Awesome",
+}).Error("Boltrus")
+```
+
+Info
+----
+Each log are separated into different db files.
+
+Boltrus will make db files in the declared path. This is the db list:
+* panic_log.db
+* fatal_log.db
+* error_log.db
+* warning_log.db
+* info_log.db
+* debug_log.db
---
+++ hook.go
@@ -0,0 +1,186 @@
+package hook
+
+import (
+	"time"
+
+	"encoding/json"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/boltdb/bolt"
+)
+
+//Hooker for hook
+type Hooker struct {
+	DBPath  string
+	BoltMap map[string]*bolt.DB
+}
+
+var panicdb string = "panic_log.db"
+var fataldb string = "fatal_log.db"
+var errdb string = "err_log.db"
+var warndb string = "warning_log.db"
+var infodb string = "info_log.db"
+var debugdb string = "debug_log.db"
+
+//NewHook add new hook for boltdb
+func NewHook(path string) (*Hooker, error) {
+	boltHook := &Hooker{
+		DBPath: path,
+		BoltMap: map[string]*bolt.DB{
+			"panic":   openDB(path, panicdb),
+			"fatal":   openDB(path, fataldb),
+			"error":   openDB(path, errdb),
+			"warning": openDB(path, warndb),
+			"info":    openDB(path, infodb),
+			"debug":   openDB(path, debugdb),
+		},
+	}
+
+	return boltHook, nil
+}
+
+func openDB(fullPath string, dbName string) *bolt.DB {
+	db, err := bolt.Open(fullPath+dbName, 0600, nil)
+	if err != nil {
+		logrus.Errorf("Cannot open %s database", dbName)
+	}
+
+	return db
+}
+
+type logrusStash struct {
+	Type        string      `json:"type"`
+	TimeStamp   string      `json:"timestamp"`
+	Sourcehost  string      `json:"host"`
+	Message     string      `json:"message"`
+	Fields      interface{} `json:"fields"`
+	Application string      `json:"application"`
+	File        string      `json:"file"`
+	Level       string      `json:"level"`
+}
+
+//Fire logging to hook
+func (bhook *Hooker) Fire(entry *logrus.Entry) error {
+	stash := logrusStash{}
+
+	stash.Message = entry.Message
+	stash.TimeStamp = entry.Time.UTC().Format(time.RFC3339Nano)
+	stash.Level = entry.Level.String()
+	stash.Fields = entry.Data
+
+	messageByte := []byte(stash.Message)
+	keyTime := []byte(stash.TimeStamp)
+	//convert fields to json
+	dataByte, _ := json.Marshal(entry.Data)
+	dataLength := len(entry.Data)
+
+	tx, _ := bhook.BoltMap[stash.Level].Begin(true)
+	tx.CreateBucketIfNotExists(messageByte)
+	bucket := tx.Bucket(messageByte)
+	putFields(bucket, keyTime, dataByte, dataLength)
+	tx.Commit()
+
+	return nil
+}
+
+//Levels rerturn available levels in hook
+func (bhook *Hooker) Levels() []logrus.Level {
+	return []logrus.Level{
+		logrus.PanicLevel,
+		logrus.FatalLevel,
+		logrus.ErrorLevel,
+		logrus.WarnLevel,
+		logrus.InfoLevel,
+		logrus.DebugLevel,
+	}
+}
+
+func putFields(bucket *bolt.Bucket, key []byte, value []byte, dataLength int) {
+	if dataLength > 0 {
+		bucket.Put(key, value)
+	}
+}
+
+//GetPanicList return all panics available in panic boltdb
+func (bhook *Hooker) GetPanicList() []string {
+	var list []string
+
+	tx, _ := bhook.BoltMap["panic"].Begin(true)
+	tx.ForEach(func(name []byte, _ *bolt.Bucket) error {
+		list = append(list, string(name))
+		return nil
+	})
+	tx.Commit()
+
+	return list
+}
+
+//GetFatalList return all fatals available in fatal boltdb
+func (bhook *Hooker) GetFatalList() []string {
+	var list []string
+
+	tx, _ := bhook.BoltMap["fatal"].Begin(true)
+	tx.ForEach(func(name []byte, _ *bolt.Bucket) error {
+		list = append(list, string(name))
+		return nil
+	})
+	tx.Commit()
+
+	return list
+}
+
+//GetErrorList return all errors available in error boltdb
+func (bhook *Hooker) GetErrorList() []string {
+	var list []string
+
+	tx, _ := bhook.BoltMap["error"].Begin(true)
+	tx.ForEach(func(name []byte, _ *bolt.Bucket) error {
+		list = append(list, string(name))
+		return nil
+	})
+	tx.Commit()
+
+	return list
+}
+
+//GetWarningList return all warnings available in warning boltdb
+func (bhook *Hooker) GetWarningList() []string {
+	var list []string
+
+	tx, _ := bhook.BoltMap["warning"].Begin(true)
+	tx.ForEach(func(name []byte, _ *bolt.Bucket) error {
+		list = append(list, string(name))
+		return nil
+	})
+	tx.Commit()
+
+	return list
+}
+
+//GetInfoList return all info available in info boltdb
+func (bhook *Hooker) GetInfoList() []string {
+	var list []string
+
+	tx, _ := bhook.BoltMap["info"].Begin(true)
+	tx.ForEach(func(name []byte, _ *bolt.Bucket) error {
+		list = append(list, string(name))
+		return nil
+	})
+	tx.Commit()
+
+	return list
+}
+
+//GetDebugList return all debugs available in debug boltdb
+func (bhook *Hooker) GetDebugList() []string {
+	var list []string
+
+	tx, _ := bhook.BoltMap["debug"].Begin(true)
+	tx.ForEach(func(name []byte, _ *bolt.Bucket) error {
+		list = append(list, string(name))
+		return nil
+	})
+	tx.Commit()
+
+	return list
+}
